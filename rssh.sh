6739efcc53874019adb137a45d84d9336e9f9726
malachyoconnor <malachyguitar@gmail.com>
2022-12-08 20:14:32 +0000

    Fixed sudo users using the wrong key

--- rssh.sh
+++ rssh.sh
@@ -7,5 +7,5 @@ PORT=31122
 
 echo $(date) rssh.sh $PORT trying ssh reverse tunnel
 
-ssh -o ExitOnForwardFailure=yes -f -N -T -R $PORT:localhost:22 mro31@tfc-app9.cl.cam.ac.uk
+ssh -o ExitOnForwardFailure=yes -f -N -T -R $PORT:localhost:22 mro31@tfc-app9.cl.cam.ac.uk -i /home/pi/.ssh/id_rsa
 
